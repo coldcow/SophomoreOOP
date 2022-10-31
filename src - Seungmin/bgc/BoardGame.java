82a27a5a301fcfafa1c6077983707c937195ddf5
5-grains <116898750+5-grains@users.noreply.github.com>
2022-10-31 22:19:13 +0900

    push test
    1

---
+++ src - Seungmin/bgc/BoardGame.java
@@ -0,0 +1,27 @@
+package bgc;
+
+import java.util.Scanner;
+
+import mgr.Manageable;
+
+public class BoardGame implements Manageable {
+
+	@Override
+	public void read(Scanner scan) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean matches(String kwd) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src - Seungmin/bgc/Drink.java
@@ -0,0 +1,28 @@
+package bgc;
+
+import java.util.Scanner;
+
+import mgr.Manageable;
+
+public class Drink implements Manageable {
+	String type;
+	String name;
+	int price;
+	
+	@Override
+	public void read(Scanner scanner) {
+		type = scanner.next();
+		name = scanner.next();
+		price = scanner.nextInt();
+	}
+	
+	@Override
+	public void print() {
+		System.out.format("[%s]/t <%s>/t %d¿ø", name, price);
+	}
+	
+	@Override
+	public boolean matches(String keyword) {
+		return (type.contains(keyword) || name.contains(keyword) || (price + "").contains(keyword));
+	}
+}
